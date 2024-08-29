8edc9d4ac2e3ea1753f5752d8261d5677a236baf
José Alberto <132082687+ZeDaManga01@users.noreply.github.com>
2024-08-29 17:15:12 -0300

    Update Cliente
    Primeiro update do Cliente
    
    -Novos testes em conjunto do Servidor
    
    -Testes geraram erro de Broken pipe no Servidor

--- Cliente_Teste_0.1.py
+++ Cliente_Teste_0.1.py
@@ -9,7 +9,7 @@ user_in = input('Insira o endereço IP ou pressione Enter:\n')
 if user_in != '':
     HOST = user_in
 
-user_in = input('Insira a porta de conexão com o servidor ou pressione ENTER')
+user_in = input('Insira a porta de conexão com o servidor ou pressione ENTER\n')
 if user_in != '':
     PORT = int(user_in)
 
@@ -18,11 +18,25 @@ server =  (HOST, PORT)
 tcp.connect(server)
 print("Pressione Ctrl+X para sair\n")
 
-info = input('Informação para ser enviada\n')
+info = input('Informe o trecho do voo\n')
 
 while info != '\x18':
-    tcp.send(str.encode(info))
 
-    info = input('Informação para ser enviada\n')
+    tcp.sendall(str.encode(info))
+
+    disp_info = tcp.recv(1024).decode('utf-8')
+
+    if disp_info == 'S':
+        resp = input('Trecho disponivel!\n\nDeseja Comprar?\nS ou N')
+        tcp.sendall(str.encode(resp))
+        compra_resultado = tcp.recv(1024).decode('utf-8')
+        if compra_resultado == 'S':
+            print('Compra realizada\n')
+        else:
+            print('Compra não realizada')
+        
+    info = input('Informe o trecho do voo\n')
+
+
 tcp.close()
 
