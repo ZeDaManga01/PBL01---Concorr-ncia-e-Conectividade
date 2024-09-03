213f2feefed9d27f8175c54a8c27be2fc4828396
José Alberto <132082687+ZeDaManga01@users.noreply.github.com>
2024-09-02 22:44:22 -0300

    🔃 Update and rename Cliente_Teste_0.2.py to Cliente_Teste_0.3.py
    Nova Implementação no teste do cliente
     -  1° Teste de simulação simples de compra realizado com sucesso
     - Encontrado problema ao encerrar o cliente

--- Cliente_Teste_0.3.py
+++ Cliente_Teste_0.3.py
@@ -14,29 +14,26 @@ if user_in != '':
     PORT = int(user_in)
 
 server =  (HOST, PORT)
-
 tcp.connect(server)
 print("Pressione Ctrl+X para sair\n")
 
-info = input('Informe o trecho do voo\n')
+info = "a"
 
 while info != '\x18':
-
+    info = input('Informe o trecho do voo\n')
     tcp.sendall(str.encode(info))
-
     disp_info = tcp.recv(1024).decode('utf-8')
 
-    if disp_info == 'S':
-        resp = input('Trecho disponivel!\n\nDeseja Comprar?\nS ou N')
+    if disp_info == 'True':
+        resp = input('Trecho disponível!\n\nDeseja Comprar?\nS ou N\n')
         tcp.sendall(str.encode(resp))
         compra_resultado = tcp.recv(1024).decode('utf-8')
-        if compra_resultado == 'S':
+        if compra_resultado == 'True':
             print('Compra realizada\n')
         else:
-            print('Compra não realizada')
-        
-    info = input('Informe o trecho do voo\n')
+            print('Compra não realizada\n')
+    else:
+        print('Trecho indisponível\n')
 
 
 tcp.close()
-
